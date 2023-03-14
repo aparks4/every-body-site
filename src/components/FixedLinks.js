dce1004f478ceccd41d501c709b9c89cfb803f95
aparks4 <arynmparks@gmail.com>
2023-03-13 23:13:53 -0400

    responsive styling for home page

--- src/components/FixedLinks.js
+++ src/components/FixedLinks.js
@@ -1,18 +1,28 @@
 import { Link } from 'react-router-dom';
 import { BsInstagram } from 'react-icons/bs';
+import { RxEnvelopeClosed } from 'react-icons/rx';
+import { HiOutlineClipboardCopy } from 'react-icons/hi';
+import { BsClipboardCheck } from 'react-icons/bs';
+import { FaRegEnvelope } from 'react-icons/fa';
+
 
 export const FixedLinks = () => {
     return (
         <>
             <div className="fixed-links-container">
 
-                <Link to ="https://docs.google.com/forms/d/e/1FAIpQLSecKfha0f4tSz1AdUo3aenF7I-97wPvA3cGGZQmM-POyKocZQ/viewform" target="_blank" rel="noreferrer"><span style={{'font-weight': 'bold'}} >🖊</span></Link>
+                <Link to ="https://docs.google.com/forms/d/e/1FAIpQLSecKfha0f4tSz1AdUo3aenF7I-97wPvA3cGGZQmM-POyKocZQ/viewform" target="_blank" rel="noreferrer">
+                    <HiOutlineClipboardCopy />
+                    {/* <BsClipboardCheck /> */}
+                </Link>
 
                 <Link to="https://www.instagram.com/every.body.retreats/" target="_blank" rel="noreferrer">
                     <BsInstagram className="insta-icon"/>
                 </Link>
 
-                <Link to="mailto:everybodyretreats@gmail.com" target='_blank' rel='noreferrer'>✉</Link>
+                <Link to="mailto:everybodyretreats@gmail.com" target='_blank' rel='noreferrer'>
+                    <FaRegEnvelope style={{'font-size': '5vh'}} />
+                </Link>
 
             </div>
         </>
