b9a9e05efbb70d96e6a204be52cf500d9e346b6e
aparks4 <arynmparks@gmail.com>
2023-03-15 19:05:29 -0400

    color images for team photos

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -10,22 +10,22 @@ export const TeamPage = (props) => {
             <div className="team-members-container">
                 <TeamMemberCard 
                     name={'Ellie Bearnson'} 
-                    photoURL={'https://i.imgur.com/j5xx9AGl.jpg'}
+                    photoURL={'https://i.imgur.com/ZgEzVZS.jpg'}
                     blurb={'Ellie is from Salt Lake City, Utah. She is in her last year of nursing school, teaching preschool part time, and preparing to lead a humanitarian trip to Thailand this summer. During her free time, Ellie loves to hike, ski, camp, and really do anything outside. Ellie struggled with disordered eating and poor body image at a very young age, for quite a long time. She says, \"Since healing from these things, I have found so much meaning, joy, and peace in life. I\'m so grateful for the relationship I\'ve developed with my body and food, and I want others to feel the same goodness that comes from that. I hope by following along here you\'ll start to feel that same healing within yourself\".' }
                 />
                 <TeamMemberCard 
                     name={'Christine Parks'} 
-                    photoURL={'https://i.imgur.com/4iklfbxl.jpg'} 
+                    photoURL={'https://i.imgur.com/qFz4mcG.jpg'} 
                     blurb={'Christine is a southern-Oregon native, but has lived in Utah for the last 8 years. She graduated from BYU in 2021 and works as a tax accountant for PwC. Christine struggled with binge eating disorder as a teenager and started her recovery journey at age 20. It\'s one of the hardest things she\'s ever been through, but she is so grateful for the growth, healing, and empowerment she has found through recovery. When she\'s not crunching numbers at work or dismantling diet culture on social media, Christine loves the mountains, traveling, and Chick-fil-A waffle fries.She can\'t wait to share Every Body with the world!'}
                 />
                 <TeamMemberCard 
                     name={'Allison McCune'} 
-                    photoURL={'https://i.imgur.com/1ZSXZgd.jpg'}
+                    photoURL={'https://i.imgur.com/SCdg6j5.jpg'}
                     blurb={'A native of Upstate New York, Allison transplanted to Salt Lake City three years ago and has fallen in love with life in Utah. Much like the other women behind Every Body, Allison dealt with a pattern of disordered eating and body dissatisfaction from middle school through college. When she came across Intuitive Eating in 2017, her world was flipped upside down! Through her own journey she found a deep passion for helping others who struggle with similar issues and has been working in direct patient care for eating disorders since February 2020. She hopes to pursue a Master\'s in Counseling in the next couple of years. When she isn\'t at work, Allison can usually be found playing outside. She loves impulse tattoos, never taking things seriously, and overanalyzing Oscar-nominated movies. She is excited to meet and connect with everyone at the retreats this summer!'}
                 />
                 <TeamMemberCard 
                     name={'Emily Damm'} 
-                    photoURL={'https://i.imgur.com/jY5v819l.jpg'}
+                    photoURL={'https://i.imgur.com/F91fjwZ.jpg'}
                     blurb={'Emily is an Oregonian who\'s loved her chapter in Utah attending Brigham Young University. She graduates this semester! As a doula, she\'s passionate about walking alongside women as they navigate the beautiful transitions through pregnancy, birth, and motherhood. During her free time you can find Emily reading about herbs or bees, hiking, biking, or making a generous pot of soup to share. Emily started battling an eating disorder without knowing what it was. After learning more and trying to recover on her own for a long time, she took a pause on her life to seek out life changing treatment, which set her on a journey of all-in healing. She says, "It\'s been incredible to feel at home in my body again. Helping create Every Body has been quite the symbol of my next step in recovery: to help others feel that, too".'}
                 />
                 <TeamMemberCard 
