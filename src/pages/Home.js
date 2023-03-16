f8a663b6f2ae30647212fb9dcab9af9f9c462cf5
aparks4 <arynmparks@gmail.com>
2023-03-15 22:16:43 -0400

    styling for tablet screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,18 +10,10 @@ export const HomePage = () => {
     return (
         <>
         <Nav />
-        {/* <WebNav /> */}
+
         <p className="home-tagline">An impactful retreat experience helping people love the bodies they are in.</p>
 
         <div className="home-page">
-            {/* <div className="left-nav">
-                <Link to="/about">about us</Link>
-                <Link to="/team">our team</Link>
-            </div>
-            <div className="right-nav">
-                <Link to="/test">test page</Link>
-                <Link to="/retreats">retreats</Link>
-            </div> */}
 
             <div className="drawing"></div>
             <div className="blob"></div>
@@ -32,12 +24,6 @@ export const HomePage = () => {
             
 
         </div>
-        {/* <div className="furthermore">
-            <div className="deco-border"></div>
-            <div className="lorem-ipsum">
-                <p>An impactful retreat experience helping everybody understand that every body is worthy of love, respect, & care.</p>
-            </div>
-        </div> */}
         </>
     )
 }
