00f56e6a8c23509f30e19c8427ad31fb8d7b1deb
aparks4 <arynmparks@gmail.com>
2023-03-18 17:34:59 -0700

    mobile styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export const HomePage = () => {
         <>
         <Nav />
 
-        <p className="home-tagline">Helping everybody understand that <br/> <span className="every-body">every body</span><br/> is worthy of <br/> <span className="love-respect-care">love, respect, & care</span>.</p>
+        <p className="home-tagline">Helping everybody understand that <br/> <span className="every-body">every body</span><br/> is worthy of <br/> <span className="love-respect-care">love, respect,<br/> & care</span>.</p>
 
         <div className="home-page">
 
