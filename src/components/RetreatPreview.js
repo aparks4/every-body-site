4db5c175bf48789e12a435078a519f0f573b391b
aparks4 <arynmparks@gmail.com>
2023-08-12 20:41:35 -0700

    update liks from retreat page

--- src/components/RetreatPreview.js
+++ src/components/RetreatPreview.js
@@ -6,8 +6,8 @@ export const RetreatPreview = (props) => {
             <div className="img-box" style={{"backgroundImage": `url(${props.image})`}}></div>
             <h2>{props.name}</h2>
             <h3>{props.dates}</h3>
-            <p><Link to={props.register} target="_blank" rel="noreferrer">Learn More</Link></p>
-            <p><Link to={props.register} target="_blank" rel="noreferrer">Register Now</Link></p>
+            <p><Link to={props.register}>Learn More</Link></p>
+            <p><Link to={props.register}>Register Now</Link></p>
         </div>
     )
 }
