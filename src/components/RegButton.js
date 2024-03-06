3828878511749609c35e085b02720326a972c4f6
aparks4 <arynmparks@gmail.com>
2024-03-06 00:16:11 -0800

    Update registration url

--- src/components/RegButton.js
+++ src/components/RegButton.js
@@ -1,7 +1,7 @@
 export const RegButton = () => {
     return (
         <div className="reg-button">
-            <a href={"https://docs.google.com/forms/d/e/1FAIpQLSecKfha0f4tSz1AdUo3aenF7I-97wPvA3cGGZQmM-POyKocZQ/viewform"} target={"_blank"} rel={'noreferrer'}>register</a>
+            <a href={"https://everybodyretreats.regfox.com/every-body-yoga-event"} target={"_blank"} rel={'noreferrer'}>register</a>
         </div>
     )
 }
